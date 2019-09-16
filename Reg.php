7ced0262628d89adc9257e519624d030f874484b
Shady BA <shadilytn@gmail.com>
2019-09-16 22:56:36 +0300

    Update Reg.php

--- Reg.php
+++ Reg.php
@@ -1,86 +1,3 @@
-<?php 
-session_start();
-
-//initialising variables
-$username = "";
-$email = "";
-$errors = array();
-
-//connect to db
-$db = mysqli_connect('localhost','root','','madepro') or die("Could not connect to database");
-
-//Register user
-if (isset($_POST['reg_user'])) {
-
-$username = mysqli_real_escape_string($db, $_POST['username']);
-$email = mysqli_real_escape_string($db, $_POST['email']);
-$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
-//form validation
-if (empty($username)) { array_push($errors, "Username is required");}
-if (empty($email)) { array_push($errors, "Email is required");}
-if (empty($password_1)) { array_push($errors, "Password is required");}
-if ($password_1 != $password_2) { array_push($errors, "Password do not match");}
-
-// check db for existing user with same username
-$user_check_query = "SELECT * FROM user WHERE username = '$username' or email = '$email' LIMIT 1";
-
-$results = mysqli_query($db, $user_check_query);
-$user = mysqli_fetch_assoc($results);
-
-if ($user) {
-	if ($user['username'] === $username) {array_push($errors, "Username already exists");}
-    if ($user['email'] === $email) {array_push($errors, "Email ID already registered");
-    }
-}
-
-//Register the user if no error
-
-if (count($errors) == 0) {
-
-	$password = md5($password_1); //this will encrypt the password
-	$query = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
-
-	mysqli_query($db, $query);
-	$_SESSION['username'] = $username;
-	$_SESSION['success'] = "You are now logged in";
-
-	header('location: https://www.madepro.site/program');
-    }
-}
-
-//LOGIN USER
-
-if (isset($_POST['login_user'])) {
-
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$password = mysqli_real_escape_string($db, $_POST['password_1']);
-
-	if (empty($username)) {
-		$array_push($errors, "Username is required") ;
-	}
-	if (empty($password)) {
-		$array_push($errors, "Password is required") ;
-	}
-	if (count($errors) == 0) {
-
-		$password = md5($password);
-		$query = "SELECT * FROM user WHERE username= '$username' AND password= '$password' ";
-
-		$results = mysqli_query($db, $query);
-
-		if (mysqli_num_rows($results)) {
-
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "Logged in successfully";
-			header('location: https://www.madepro.site/program');
-		}else{
-			array_push($errors, "Wrong username/Password. Please try again");
-		}
-	}
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -224,14 +141,6 @@ if (isset($_POST['login_user'])) {
 	<div class="col-md-7 register-right">	
 	<form class="registration">
 	<form action="registration.php" method="post">
-		
-			<?php if(count($errors) > 0 ) : ?>
-	<div>
-		<?php foreach ($errors as $error) :?>
-			<p><?php echo $error ?></p>
-		<?php endforeach ?>	
-	</div>
-<?php endif?>
 		
 		<div>
 				<label for="username"> Username : </label>
